5b220ce74d5da1668997ee43120baf6a81bbc6a9
Taewan Kang <ktw_1016@hotmail.com>
2020-03-18 10:37:35 -0400

    anchor tag to open securely and new tab

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,12 +10,12 @@ const About = () => (
     <div style={{ display: "flex", flexDirection: "column", marginBottom: 30 }}>
       <h2> CREDITS </h2>
       <span dangerouslySetInnerHTML={{
-        __html: "The graph of Canada was inspired from <a href='https://github.com/TBS-EACPD/infobase/blob/master/LICENSE'>GC Infobase </a>",
+        __html: "The graph of Canada was inspired from <a href='https://github.com/TBS-EACPD/infobase/blob/master/LICENSE' target='_blank' rel='noopener noreferrer'> GC Infobase </a>",
       }} />
       <br></br>
       <h2> CONTACT </h2>
       <span dangerouslySetInnerHTML={{
-        __html: "You can reach me on <a href='https://www.taewankang.com'> my website </a>",
+        __html: "You can reach me on <a href='https://www.taewankang.com' target='_blank' rel='noopener noreferrer'> my website </a>",
       }} />
 
     </div>
