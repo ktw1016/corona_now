30c119f0ae4c3f9d73354aa253c299eba69f1f02
Taewan Kang <ktw_1016@hotmail.com>
2020-03-23 11:12:15 -0400

    make fetch data async

--- src/data/fetch_data.js
+++ src/data/fetch_data.js
@@ -5,36 +5,38 @@ const fs = require('fs');
 const confirmed_file = "time_series_19-covid-Confirmed.csv";
 const deaths_file = "time_series_19-covid-Deaths.csv";
 const lastUpdated_file = "lastUpdated.csv";
-const confirmed_cmd = `curl -H 'Authorization: token 89830a265aa6f38256bd73cfe15c951aacceca0c' -H 'Accept: application/vnd.github.v3.raw' -O -L https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/${confirmed_file}`;
-const deaths_cmd = `curl -H 'Authorization: token 89830a265aa6f38256bd73cfe15c951aacceca0c' -H 'Accept: application/vnd.github.v3.raw' -O -L https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/${deaths_file}`;
+const pat_cmd = "echo $GIT_PAT";
+var pat = "";
 
-const download_and_push_data = () => {
-  exec(confirmed_cmd, function(error, stdout, stderr){
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if(error !== null)
-    {
-      console.log('exec error: ' + error);
-    }
-  });
-  exec(deaths_cmd, function(error, stdout, stderr){
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if(error !== null)
-    {
-      console.log('exec error: ' + error);
-    }
-  });
-  simpleGit()
-    .add([confirmed_file, deaths_file, lastUpdated_file])
-    .commit("Auto Data Update", () => exec(`git push --set-upstream origin master`, function(error, stdout, stderr){
+const download_and_push_data = async () => {
+  exec(pat_cmd, function(error, stdout, stderr){
+    pat = stdout.trim();
+    const confirmed_cmd = `curl -H 'Authorization: token ${pat}' -H 'Accept: application/vnd.github.v3.raw' -O -L https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/${confirmed_file}`;
+    const deaths_cmd = `curl -H 'Authorization: token ${pat}' -H 'Accept: application/vnd.github.v3.raw' -O -L https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/${deaths_file}`;
+    if(error !== null){ console.log('exec error: ' + error); }
+    exec(confirmed_cmd, function(error, stdout, stderr){
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
-      if(error !== null)
-      {
-        console.log('exec error: ' + error);
-      }
-    }));
+      if(error !== null) { console.log('exec error: ' + error); }
+      exec(deaths_cmd, function(error, stdout, stderr){
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if(error !== null) { console.log('exec error: ' + error); }
+        simpleGit()
+          .removeRemote("origin")
+          .addRemote("origin", `https://ktw1016:${pat}@github.com/ktw1016/corona_now.git`)
+          .add([confirmed_file, deaths_file, lastUpdated_file])
+          .commit("Auto Data Update", () => exec(`git push --set-upstream origin master`, function(error, stdout, stderr){
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if(error !== null)
+            {
+              console.log('exec error: ' + error);
+            }
+          }));
+      });    
+    });
+  });
 };
 
 const getToday = () => {
@@ -54,13 +56,11 @@ const write_last_updated_to_CSV = () => {
   console.log("Last Updated CSV written");
 };
 
-download_and_push_data();
-write_last_updated_to_CSV();
 console.log("Start polling..");
-
 setInterval(async () => {
-  await download_and_push_data();
-  await write_last_updated_to_CSV();
-  
-  console.log(`Finished polling at: ${getToday()}`);
+  await download_and_push_data().then( async () => {
+    await write_last_updated_to_CSV().then( () => {
+      console.log(`Finished polling at: ${getToday()}`);
+    });
+  });
 }, 1 * 60 * 60 * 1000); //every hour, download, push data and write last updated CSV
